6f71a6fafefa391a70963b1354b473f4b6929207
Chris <44699644+ForgeAus@users.noreply.github.com>
2022-09-02 11:54:45 +1000

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,16 +1,61 @@
-""" Random List Printer"""
-
+""" Random Generator """
+ 
 import random
-
-list_items = []
-
-list_len = int( input ("How long is the list: \n") )
-
-for make_list in range( list_len ) :
-    list_items.append( input( "Type the next list item : \n" ) )
-
-gencount = int( input( "Type how many items to generate: \n" ) )
-
-print( "\nThe items in the list are : \n" )
-for print_count in range(gencount) :
-    print( list_items[int( random.randint( 0, ( list_len - 1 ) ) )] )
+ 
+class TemplateError( Exception ):
+    """Key error for when no matching listnames are found"""
+ 
+class NotANumber( Exception ):
+    """Type or Value error for when the input was not a number"""
+ 
+lists = {}
+ 
+def num_entry( question ):
+    """A function that takes in a number and assigns it to an integer"""
+    result = 0
+    entry = ""
+    
+    while result < 1:
+        
+        entry = input( question )
+        try:
+            result = int( entry )            
+        except (TypeError, ValueError):
+            raise NotANumber( f"Sorry, unable to get a number from {entry}" )
+ 
+    return result
+ 
+num_lists = num_entry( "Please enter how many lists:" )
+ 
+for list_count in range( num_lists ):
+    list_name = input( "\nPlease enter a unique title for this list: \n" )
+    lists[list_name] = []
+    
+    print( f"Please add a new entry to {list_name} or use an empty line to end the list: \n" )
+    while True:
+        item = input()
+    
+        if item == "":
+            break
+        
+        lists[list_name].append( item )
+ 
+ 
+gen_count = num_entry( "Please enter how many items to generate: \n" )
+ 
+gen_format = input( "Please enter a template: \n(for example): This random character has <hair_color> hair and <eye_color> eyes.\n" )
+ 
+print( "\nThe Generated items are : \n" )
+ 
+for gen_index in range( gen_count ):
+    
+    gen_string = gen_format
+    for field in lists:
+        try:
+            while (f"<{field}>") in gen_string:
+                gen_string = gen_string.replace( f"<{field}>", random.choice( lists[field] ), 1 )
+        except KeyError:
+            raise TemplateError( f"Cannot find a list called <{field}> \n" )
+    
+    print( gen_string )
+    
